6c72da10aea41104e5e7a265b2776e514ea5449a
bipinkishore <bipinkpayyavulla@gmail.com>
2021-10-10 19:25:05 +0530

    F1: Added java code for CSV processing and displaying the data on console
    Files Modified:
    Test\src\com\inmar\code\Person.java
    Test\src\com\inmar\code\PersonImporter.java
    
    Added File:
    Test\persons.csv

--- Test/src/com/inmar/code/Person.java
+++ Test/src/com/inmar/code/Person.java
@@ -2,11 +2,19 @@ package com.inmar.code;
 
 import java.util.UUID;
 
+/*Person Class, which has three member variables, a String for first name, a
+String for last name, and a UUID as a unique identifier*/
 public class Person {
-	public String firstName;
 	public String lastName;
+	public String firstName;
 	public UUID uuId;
 	
+	public Person(String firstName2, String lastName2, UUID uUID2) {
+		// TODO Auto-generated constructor stub
+		this.firstName=firstName2;
+		this.lastName=lastName2;
+		this.uuId=uUID2;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
@@ -25,7 +33,4 @@ public class Person {
 	public void setUuId(UUID uuId) {
 		this.uuId = uuId;
 	}
-	
-	
-
 }
--- Test/src/com/inmar/code/PersonImporter.java
+++ Test/src/com/inmar/code/PersonImporter.java
@@ -1,27 +1,67 @@
 package com.inmar.code;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class PersonImporter {
-	
+
 	static PersonPublisher personPublisher = null;
-	
-	public static void main(String[] args)
-	 { 
-		List<Person> persons = readPersonsFromfFile("persons.txt"); // Read data from file
-		if(persons.size()>0){
+
+	public static void main(String[] args) {
+		List<Person> persons = readPersonsFromfFile("persons.csv"); // Read data from file
+		if (persons.size() > 0) {
 			personPublisher = new PersonPublisher();
-			for (Person p : persons) 
-			 { 
+			for (Person p : persons) {
 				personPublisher.publishMessage(p);
-			 } 
+			}
 		}
-	 
+
 	}
 
-	private static List<Person> readPersonsFromfFile(String string) {
-		// TODO Auto-generated method stub
+	private static List<Person> readPersonsFromfFile(String fileName) {
+		List<Person> persons = new ArrayList<>();
+		Path pathToFile = Paths.get(fileName); 
+		try (BufferedReader br = Files.newBufferedReader(pathToFile,
+				StandardCharsets.US_ASCII)) { 
+			// read the first line from the text file
+			String line = br.readLine(); 
+			// loop until all lines are read
+			while (line != null) { 
+				// use string.split to load a string array with the values from each line of  the file, using a comma as the delimiter
+				String[] attributes = line.split(",");
+				Person person = createPerson(attributes); 
+				// adding Person into arrayList
+				if(person !=null){
+					persons.add(person); 
+					
+				}
+				// read next line before looping if end of file reached, line would be null
+				line = br.readLine();
+			}
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		return persons;
+	}
+
+	private static Person createPerson(String[] metadata) {
+		try{
+		String firstName = metadata[0];
+		String lastName = metadata[1];
+		UUID uUID = UUID.fromString(metadata[2]); // create and return Person of this metadata
+		return new Person(firstName, lastName, uUID);
+		}
+		catch(java.lang.IllegalArgumentException e){
+			e.printStackTrace();
+		}
 		return null;
-	} 
+	}
 
 }
